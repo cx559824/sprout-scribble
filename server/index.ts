651e96fd06ec2df451cc2555cb299a1572108868
rodolfo-ud <rodolfo@undetectable.ai>
2024-09-01 02:40:38 +0800

    drizzle set up

---
+++ server/index.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "@/server/schema";
+
+config({ path: ".env" }); // or .env.local
+
+const sql = neon(process.env.POSTGRES_URL!);
+
+export const db = drizzle(sql, { schema, logger: true });
---
+++ server/migrations/0001_premium_garia.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "posts" ADD COLUMN "shit" text NOT NULL;
